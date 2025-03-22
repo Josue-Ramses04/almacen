3b9998d2faed9351dc35c81290bf7d01a612237c
Josue Ramses Rojas Vallejo <ramses@MacBook-Air-de-Ramses.local>
2025-03-22 00:39:50 -0600

    Statics

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-c8jenlwr+q^zjc#c^5f@z5aklwoh-%pt)i1mwkwwnzx0#m62o=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS=['localhost', 'almacen-production-b1b7.up.railway.app']
+ALLOWED_HOSTS=['localhost', 'almacen-production-b1b7.up.railway.app' , 'localhost']
 
 JAZZMIN_SETTINGS = {
     
