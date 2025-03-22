1579eaf4fa69b2a9e4c4da04a73285f3af843a47
Josue Ramses Rojas Vallejo <ramses@MacBook-Air-de-Ramses.local>
2025-03-22 00:32:48 -0600

    CSRF

--- core/settings.py
+++ core/settings.py
@@ -165,3 +165,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+CSRF_TRUSTED_ORIGINS = ['https://almacen-production-b1b7.up.railway.app']
