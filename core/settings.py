9908c696ec17dcbeab1cb73ab7899be5a305c5ee
Josue Ramses Rojas Vallejo <ramses@MacBook-Air-de-Ramses.local>
2025-03-21 21:47:18 -0600

    databases MYSQL

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -98,16 +102,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "sql_injection",
-        "USER": "root",
-        "PASSWORD": "rojas2004",
-        "HOST": "localhost",
-        "PORT": "3306",
-    }
+    "default": dj_database_url.config(default=os.getenv)
 }
+
+
+
+
+
+
+
 
 
 # Password validation
